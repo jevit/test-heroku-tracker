3ea3464ff4a6b7022d33d2773c08ebd9e14e7749
jevit <jeremie.viteau@gmail.com>
2017-09-04 10:46:08 +0200

    git

--- src/main/java/jv/jpatpl/Spring.java
+++ src/main/java/jv/jpatpl/Spring.java
@@ -7,9 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class Spring {
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring.class, args);
